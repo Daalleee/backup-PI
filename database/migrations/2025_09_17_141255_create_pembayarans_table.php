268d3f41cf7d020718c2580824247b52742de9d7
Daalleee <dalemasan10@gmail.com>
2025-09-26 04:54:27 +0700

    buat model pembayaran

--- database/migrations/2025_09_17_141255_create_pembayarans_table.php
+++ database/migrations/2025_09_17_141255_create_pembayarans_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pembayarans', function (Blueprint $table) {
-            $table->id();
+            $table->string('id_pembayaran')->primary();
+            $table->string('id_transaksi');
+            $table->double('jumlah_bayar');
+            $table->string('status_bayar')->default('Belum Lunas');
             $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('id_transaksi')->references('id_transaksi')->on('transaksis')->onDelete('cascade');
         });
     }
 
